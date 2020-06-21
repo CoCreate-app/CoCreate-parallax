ae75cc056ffccf660ec2095f80203c843cbeb12e
Jean Mendoza <janco1187@gmail.com>
2020-06-21 22:56:54 +0000

    minified

---
+++ src/CoCreate-parallax.js
@@ -0,0 +1,15 @@
+
+function setparallax()
+    {
+      var parallaxEls = document.getElementsByClassName("parallax");
+    
+    for (var i=0; i<parallaxEls.length; i++) {
+    		  var parallaxEl = parallaxEls.item(i);
+    		  
+    		  parallaxEl.style.backgroundImage = 'url('+parallaxEl.getAttribute('data-parallax_src')+')';
+    		  parallaxEl.style.height = parallaxEl.getAttribute('data-parallax_height');
+    	}
+    	
+    }
+    
+    setparallax();
