d704ab3c1ef532d40acb6f388f045129fd47d10a
Frank <frank@cocreate.app>
2022-02-24 19:10:28 +0000

    fix: CoCreate.config replace CoCreate.app with *

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/parallax/index.html",
                 "domains": [
-                    "cocreate.app",
+                    "*",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/parallax",
