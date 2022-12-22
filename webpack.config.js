6a2c332e0464ccb817a3a21ef5ca1d4879ee7df1
Frank Pagan <frank@cocreate.app>
2022-12-22 13:22:46 +0000

    fix: update demos and bump dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
     libraryExport: "default",
     library: ["CoCreate", "parallax"],
     globalObject: "this",
-    // publicPath: 'https://server.cocreate.app/CoCreateJS/dist/'
   },
 
   plugins: [
