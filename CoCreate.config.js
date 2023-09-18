7fac16f41bc15b1e79990ac194622a1ad00290bf
frankpagan <frank@cocreate.app>
2023-09-17 19:11:35 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "60208f6c3c260207bf49cabc",
                 "name": "index.html",
-                "path": "/docs/parallax/index.html",
+                "path": "/docs/parallax",
+                "pathname": "/docs/parallax/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/parallax",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "parallax",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
