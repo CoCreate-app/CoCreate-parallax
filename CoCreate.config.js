7ff6f353c91664f2f0115ebe5f5c511af229c65b
frankpagan <frank@cocreate.app>
2023-11-07 09:47:45 -0600

    fix: update host

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -12,8 +12,7 @@ module.exports = {
                 "pathname": "/docs/parallax/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
-                    "*",
-                    "general.cocreate.app"
+                    "*"
                 ],
                 "directory": "parallax",
                 "content-type": "{{content-type}}",
